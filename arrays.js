2efd038f9fe40038448888983cba5788b3434582
Christian Weiss <worldmover777@gmail.com>
2018-01-27 22:58:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,8 @@ function addElementToEndOfArray(array,element){
 }
 
 function destructivelyAddElementToEndOfArray(array,element){
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array,index){
@@ -22,7 +23,8 @@ function accessElementInArray(array,index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array){
@@ -30,7 +32,8 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop()
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array){
