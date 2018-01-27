4edcc8e225095c0cc1c79d191a993dea0d69827e
Christian Weiss <worldmover777@gmail.com>
2018-01-27 22:21:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,4 +32,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array.pop()
 }
 
-function 
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length - 1)
+}
